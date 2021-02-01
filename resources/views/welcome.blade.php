24d73c846e9eaa3fd2ed04ad02ad444f2807d404
jfloreso <jfloreso@sre.gob.mx>
2021-01-31 22:13:33 -0600

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel del George
+                    Laravel del Georges
                 </div>
 
                 <div class="links">
